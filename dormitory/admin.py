66d12dd70c72612c27335761e888e8ac311b8aa3
Nodirbek <nodirbekaxmadaliyev4888@gmail.com>
2025-06-11 10:11:32 +0500

    Modellar yaratildi

--- dormitory/admin.py
+++ dormitory/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Dormitory, Device
 
-# Register your models here.
+
+@admin.register(Dormitory)
+class DormitoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'director_full_name')
+    search_fields = ('name', 'address', 'director__user__first_name', 'director__user__last_name')
+
+    def director_full_name(self, obj):
+        return obj.director.user.get_full_name()
+    director_full_name.short_description = "Direktor"
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dormitory_name', 'ipaddress', 'username', 'entrance')
+    search_fields = ('ipaddress', 'username', 'dormitory__name')
+    list_filter = ('entrance',)
+
+    def dormitory_name(self, obj):
+        return obj.dormitory.name
+    dormitory_name.short_description = "Yotoqxona"
